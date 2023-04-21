e6cb8a7c47b76798081e36f1aae9b3e6b241fed6
lclob <116601056+lclob@users.noreply.github.com>
2023-04-20 23:28:20 -0300

    update styles

--- views/producto.php
+++ views/producto.php
@@ -7,57 +7,55 @@ $vinilo = producto_x_id($id);
 
 <div id="producto" class="mb-5">
 
-  <div class="container">
-    <h1 class="text-center mb-5 fw-bold text-white">disco-store</h1>
-  </div>
-
-  <div class="container">
-    <div class="row align-items-center justify-content-center">
-      
-      <?PHP if (!empty($vinilo)) { ?>
-        <div class="col">
-          <figure>
-            <img src="assets/img/<?= $vinilo['portada'] ?>" alt="imagen producto">
-          </figure>
-        </div>
-        <div class="col descripcion-producto">
-          <p class="fs-6 fw-bold artista">
-            <?= $vinilo['artista'] ?>
-          </p>
-          <h2>
-            <?= $vinilo['titulo'] ?>
-          </h2>
-          <div class="fs-3 mb-3 fw-bold text-start precio">$
-            <span><?= number_format($vinilo['precio'], 2, ",", ".") ?></span>
-          </div>
-          <p>
-            <?= $vinilo['bajada'] ?>
-          </p>
-          <ul class="list-group list-group-flush bg-dark">
-            <li class="list-group-item px-0 bg-dark"><span class="fw-bold">Genero:</span>
-              <?= $vinilo['genero'] ?>
-            </li>
-            <li class="list-group-item px-0 bg-dark"><span class="fw-bold">Producción:</span>
-              <?= $vinilo['productor'] ?>
-            </li>
-            </li>
-            <li class="list-group-item px-0 bg-dark"><span class="fw-bold">Canciones:</span>
-              <?= $vinilo['canciones'] ?>
-            </li>
-            <li class="list-group-item px-0 bg-dark"><span class="fw-bold">Publicación:</span>
-              <?= $vinilo['publicacion'] ?>
-            </li>
-          </ul>
-          <a href="#" class="mb-3 btn bg-btn w-100 fw-bold">COMPRAR</a>
-        </div>
+  <div>
+    <div class="container">
+      <h1 class="text-center mb-5 fw-bold text-white">disco-store</h1>
     </div>
+    <div class="container">
+      <div class="row align-items-center justify-content-center">
+        <?PHP if (!empty($vinilo)) { ?>
+          <div class="col">
+            <figure>
+              <img src="assets/img/<?= $vinilo['portada'] ?>" alt="imagen producto">
+            </figure>
+          </div>
+          <div class="col descripcion-producto">
+            <p class="fs-6 fw-bold artista">
+              <?= $vinilo['artista'] ?>
+            </p>
+            <h2>
+              <?= $vinilo['titulo'] ?>
+            </h2>
+            <div class="fs-3 mb-3 fw-bold text-start precio">$
+              <span><?= number_format($vinilo['precio'], 2, ",", ".") ?></span>
+            </div>
+            <p>
+              <?= $vinilo['bajada'] ?>
+            </p>
+            <ul class="list-group list-group-flush bg-dark">
+              <li class="list-group-item px-0 bg-dark"><span class="fw-bold">Genero:</span>
+                <?= $vinilo['genero'] ?>
+              </li>
+              <li class="list-group-item px-0 bg-dark"><span class="fw-bold">Producción:</span>
+                <?= $vinilo['productor'] ?>
+              </li>
+              </li>
+              <li class="list-group-item px-0 bg-dark"><span class="fw-bold">Canciones:</span>
+                <?= $vinilo['canciones'] ?>
+              </li>
+              <li class="list-group-item px-0 bg-dark"><span class="fw-bold">Publicación:</span>
+                <?= $vinilo['publicacion'] ?>
+              </li>
+            </ul>
+            <a href="#" class="mb-3 btn bg-btn w-100 fw-bold">COMPRAR</a>
+          </div>
+        </div>
+      </div>
+    <?PHP } else { ?>
+      <div class="col">
+        <h2 class="text-center m-5">No se encontró el producto deseado.</h2>
+      </div>
+    <?PHP } ?>
   </div>
-<?PHP } else { ?>
-  <div class="col">
-    <h2 class="text-center m-5">No se encontró el producto deseado.</h2>
-  </div>
-<?PHP } ?>
-
-</div>
-</div>
+  
 </div>
